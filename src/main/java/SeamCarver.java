4632d97adec749956d00dd3072b84b6f7fed7fde
Harinando Andrianarimanana <toni.andrianaht@gmail.com>
2017-12-15 01:59:49 -0800

    Compute StreamCaver

--- src/main/java/SeamCarver.java
+++ src/main/java/SeamCarver.java
@@ -2,52 +2,77 @@ import edu.princeton.cs.algs4.Picture;
 
 public class SeamCarver {
 
+    private Picture picture;
+    private double [][] energy;
     /*
      * create a seam carver object based on the given picture
      */
-    public SeamCarver(Picture picture) {
+    public SeamCarver(final Picture picture) {
+        this.picture = picture;
+
+        energy = new double[width()][height()];
+
+        for (int x = 0; x < width(); x++) {
+            for (int y = 0; y < height(); y++) {
+                energy[x][y] = energy(x, y);
+            }
+        }
     }
 
     /*
      * current picture
      */
     public Picture picture() {
-        return null;
+        return picture;
     }
 
     /*
      * width of current picture
      */
     public int width() {
-        return 0;
+        return picture.width();
     }
 
     /*
      * height of current picture
      */
     public int height() {
-        return 0;
+        return picture.height();
     }
 
     /*
-     * energy of pixel at column x and row y
+     * @param x
+     *    column of picture
+     * @param y
+     *    row of picture
+     * @return
+     *    energy of pixel at column x and row y
      */
-    public double energy(int x, int y) {
-        return 0L;
+    public double energy(final int x, final int y) {
+        return DualGradientEnergyCalculator.energy(picture, x, y);
     }
 
     /*
      * Sequence of indices for horizontal seam
      */
     public int[] findHorizontalSeam() {
-        return null;
+        // Initialize transposed energy - Abstract transpose later.
+        double[][] transposedEnergy = new double[height()][width()];
+        for (int row = 0; row < height(); row++) {
+            for (int col = 0; col < width(); col++) {
+                transposedEnergy[row][col] = energy[col][row];
+            }
+        }
+        final StreamCarverHelper streamFinder = new StreamCarverHelper(transposedEnergy, width(), height());
+        return streamFinder.findVerticalStream();
     }
 
     /*
      * sequence of indices for vertical seam
      */
     public int[] findVerticalSeam() {
-        return null;
+        final StreamCarverHelper streamFinder = new StreamCarverHelper(energy, height(), width());
+        return streamFinder.findVerticalStream();
     }
 
     /*
@@ -59,6 +84,6 @@ public class SeamCarver {
     /*
      * remove vertical seam from current picture
      */
-    public    void removeVerticalSeam(int[] seam) {
+    public void removeVerticalSeam(int[] seam) {
     }
 }
---
+++ src/main/java/StreamCarverHelper.java
@@ -0,0 +1,122 @@
+public class StreamCarverHelper {
+
+    private double[][] energy;
+    private int width;
+    private int height;
+
+    private double[][] distTo;
+    private int[][] edgeTo;
+
+    public StreamCarverHelper(final double[][] energy, final int height, final int width) {
+        this.energy = energy;
+        this.width = width;
+        this.height = height;
+
+        this.edgeTo = new int[width][height];
+        this.distTo = new double[width][height];
+        initialize();
+    }
+
+    /*
+     * Relax
+     */
+    public int[] findVerticalStream() {
+
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+
+                // relax bottom-right if it is valid
+                relax(col, row, col+1, row+1);
+
+                // relax bottom
+                relax(col, row, col, row+1);
+
+                // relax bottom-left
+                relax(col, row, col-1, row+1);
+            }
+        }
+
+        return pathTo();
+    }
+
+    private int[] pathTo() {
+        int minIndex = -1;
+        double minVal = Double.MAX_VALUE;
+        for (int col = 0; col < width; col++) {
+            final double minValCandidate = distTo[col][height-1];
+            if (minValCandidate < minVal) {
+                minVal = minValCandidate;
+                minIndex = getIndex(col, height-1);
+            }
+        }
+
+        int nextIndex = minIndex;
+        int[] path = new int[height];
+        int posPtr = height - 1;
+
+        while(nextIndex != -1) {
+            int lastCol = getColumnIndex(nextIndex);
+            int lastRow = getRowIndex(nextIndex);
+            nextIndex = edgeTo[lastCol][lastRow];
+            path[posPtr] = lastCol;
+            posPtr--;
+        }
+        return path;
+    }
+
+
+    private void relax(final int fromCol, final int fromRow, final int toCol, final int toRow) {
+        if (fromCol < 0 || fromRow < 0 || toCol < 0 || toRow < 0)
+            return;
+
+        if (fromCol > width - 1 || fromRow > height - 1 || toCol > width - 1 || toRow > height - 1)
+            return;
+
+        final double distToCandidate = distTo[fromCol][fromRow] + energy[toCol][toRow];
+        if (distToCandidate < distTo[toCol][toRow]) {
+            distTo[toCol][toRow] = distToCandidate;
+            edgeTo[toCol][toRow] = getIndex(fromCol, fromRow);
+        }
+    }
+
+    /*
+     * Initialize distTo to Infinity
+     */
+    private void initialize() {
+        // Initialize first row
+        for (int col = 0; col < this.width; col++) {
+            this.edgeTo[col][0] = -1;
+            this.distTo[col][0] = energy[col][0];
+        }
+
+        // Initialize the rest of the rows
+        for (int row = 1; row < this.height; row++) {
+            for (int col = 0; col < width; col++) {
+                this.edgeTo[col][row] = -1;
+                this.distTo[col][row] = Double.MAX_VALUE;
+            }
+        }
+    }
+
+    /*
+     * Get 1D index from pixels
+     *
+     */
+    private int getIndex(final int col, final int row) {
+        return width * row + col;
+    }
+
+    /*
+     *  Get the column index from 1D index
+     */
+    private int getColumnIndex(final int pixelIndex) {
+        return pixelIndex % width;
+    }
+
+    /*
+     * Get the row index from 1D index
+     */
+    private int getRowIndex(final int pixelIndex) {
+        return pixelIndex / width;
+    }
+}
